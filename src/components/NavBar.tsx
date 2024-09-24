069b62be6345d2f28521c4b1ebec28a11009f55c
FallingSakura <dbw1732351264@163.com>
2024-09-24 12:07:04 +0800

    trendings & cards

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,36 @@
+'use client'
 import styles from '../styles/NavBar.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
-function NavBar () {
+import { useState } from 'react'
+function NavBar() {
   const navList: Array<string> = ['NEWS', 'CATEGORIES', 'ARCHIVES', 'SUBSCRIBE']
+  const [hovered, setHovered] = useState(false)
   return (
     <div className={styles.container}>
       <Link href="/">
         <div className={`${styles.title} ${styles['no-select']}`}>
-          <Image src="/favicon.ico" alt="icon" width="50" height="50"/>
+          <Image src="/favicon.ico" alt="icon" width="50" height="50" />
           <h1>NEWS</h1>
         </div>
       </Link>
       <div className={`${styles.nav} ${styles['no-select']}`}>
-        {navList.map((item, index) => (
-          <Link className={styles.item} href={`/${item.toLowerCase()}`} key={index}>
-            {item}
-          </Link>
-        ))}
+        {navList.map((item, index) => {
+          return (
+            <Link
+              className={styles.item}
+              href={`/${item.toLowerCase()}`}
+              key={index}
+              onMouseOver={() => item === 'NEWS' && setHovered(true)}
+              onMouseLeave={() => item === 'NEWS' && setHovered(false)}
+            >
+              {item}
+              {hovered && item === 'NEWS' && (
+                <div className={styles.panel}></div>
+              )}
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
--- src/components/TrendingsArea.tsx
+++ src/components/TrendingsArea.tsx
@@ -1,7 +1,63 @@
+'use client'
 import styles from '../styles/TrendingsArea.module.css'
-function TrendingsArea () {
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowTrendUp } from '@fortawesome/free-solid-svg-icons'
+interface TrendingItem {
+  title?: string
+  date?: string
+  link?: string
+}
+function TrendingsArea() {
+  const [data] = useState<Array<TrendingItem>>([
+    {
+      title: "UN Condemns 'Abhorrent' Terrorist Attacks in Pakistan",
+      date: 'September 30, 2023',
+      link: 'https://www.globalissues.org/news/2023/09/30'
+    },
+    {
+      title: 'New Global Framework Adopted to Reduce Harmful Chemicals',
+      date: 'September 30, 2023',
+      link: 'https://www.globalissues.org/news/2023/09/30'
+    },
+    {
+      title: 'AIADMK Quits BJP-Led NDA Alliance in Tamil Nadu',
+      date: 'September 25, 2023',
+      link: 'https://news.abplive.com/india-news/top-news-headlines-25-september-2023-1631109'
+    },
+    {
+      title:
+        'U.S. House Speaker Kevin McCarthy Launches Impeachment Inquiry Against President Biden',
+      date: 'September 12, 2023',
+      link: 'https://www.infoplease.com/current-events/september-2023-news'
+    },
+    {
+      title: 'Texas Ordered to Move Border Buoys',
+      date: 'September 6, 2023',
+      link: 'https://www.theguardian.com/us-news/2023/sep/06'
+    }
+  ])
   return (
-    <div className={styles.trendings}></div>
+    <div className={styles.trendings}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Trendings</h1>
+        <FontAwesomeIcon icon={faArrowTrendUp} className={styles.icon} />
+      </div>
+      <div className={styles.content}>
+        <ol>
+          {
+            data.map((item, index) => {
+              return (
+                <li className={styles.listItem} key={index}>
+                  <a href={item.link}>{item.title}</a>
+                  <p>{item.date}</p>
+                </li>
+              )
+            }) 
+          }
+        </ol>
+      </div>
+    </div>
   )
 }
 export default TrendingsArea
